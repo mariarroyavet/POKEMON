91c35b50e2248286ea7f33424fb32df861f86cb2
luuribe <luuribe@bancolombia.com.co>
2023-06-02 18:14:20 -0500

    API de pokemones

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -1,54 +1,25 @@
-/*const userName = window.prompt ("What is your name?")
-console.log(userName);
-if (userName === "Admin") {
-    const userPassword = window.prompt ("What is your password?")
-
-    if (!userName){ //! null or '' --> false --> !false --> true  
-        console.log("Cancelado");
-    } else if (userName !== "Admin") {
-        console.log ("No te conozco")
-    } else 
-    {const userPassword = window.prompt ("What is your password?")}
-
-    if (!userPassword) {
-        console.log("Cancelado");
-    } else if (userPassword !== "TheMaster"){
-        console.log ("Contraseña Incorrecta");
-    } else {
-        console.log("Bienvenido!");
-    }
-
-}*/
-
 const url = 'https://pokeapi.co/api/v2/pokemon';
+let pokemones = [];
 
-const getPokemons = async () => {
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-        return data.results;
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-const pokemons = getPokemons().then((data) => {
-    console.log(data);
-});
-
-/*const parentElement = document.getElementById('container');
+//const parentElement = document.getElementById('container'); // Corregir de acuerdo al nuevo ejemplo
 
 const fetchApi = async (url) => {
     try {
         const res = await fetch(url);
         const data = await res.json();
-        data.results.forEach(element => { ( //AQUÍ DEBO AGREGAR LA RUTA DE CADA IMAGEN QUE ESTÁ EN EL NAME 
-            const newElement = document.createElement('p');
-            const img = document.ccreateElement('img');
+        data.results.forEach(async element => { //AQUÍ DEBO AGREGAR LA RUTA DE CADA IMAGEN QUE ESTÁ EN EL NAME 
+            /*const newElement = document.createElement('p');
+            const img = document.createElement('img');
             newElement.textContent = `nombre :${element.name}`
             img.src = element.image;
             parentElement.appendChild(img);
-            parentElement.appendChild(newElement);        
+            parentElement.appendChild(newElement);*/
+            const pokemonInfoUrl = url + '/' + element.name;
+            const pokemonInfo = await fetch(pokemonInfoUrl);
+            const pokemonInfoJson = await pokemonInfo.json();
+            const pokemonImg = pokemonInfoJson.sprites.front_default;
+            console.log(element.name);
+            console.log(pokemonImg);
         });
     } catch (error) {
         const errorMsg = document.createElement('p');
