5cfb2406f14d0e264ea22ff1dc3fbb051099b002
Mariana <mariarroyave05@gmail.com>
2023-07-04 22:39:52 -0500

    fetching correctly

--- root/scripts/cards.js
+++ root/scripts/cards.js
@@ -1,37 +1,35 @@
 const url = "https://pokeapi.co/api/v2/pokemon";
 
-const parentElement = document.getElementById('.container'); // Corregir de acuerdo al nuevo ejemplo
+const parentElement = document.getElementById(".container"); // Corregir de acuerdo al nuevo ejemplo
 
 const fetchApi = async (url) => {
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-        data.results.forEach(async (element) => { //AQUÍ DEBO AGREGAR LA RUTA DE CADA IMAGEN QUE ESTÁ EN EL NAME 
-            const response = await fetch(element.url);
-            const infoPokemon = await response.json();
-            //console.log(element.name);
-            const pokemonImg = infoPokemon.sprites.other["home"].front_default;
-            //console.log(pokemonImg);
-            const pokemonExp = infoPokemon.base_experience;
-            //console.log(pokemonExp);
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    data.results.forEach(async (element) => {
+      //AQUÍ DEBO AGREGAR LA RUTA DE CADA IMAGEN QUE ESTÁ EN EL NAME
+      const response = await fetch(element.url);
+      const infoPokemon = await response.json();
 
-            const newCard = document.createElement("div");
-            newCard.className = "card";
-
-            
-            /*const newElement = document.createElement('p');
-            const img = document.createElement('img');
-            newElement.textContent = `nombre :${element.name}`
-            img.src = element.image;
-            parentElement.appendChild(img);
-            parentElement.appendChild(newElement);
-*/
-        });
-    } catch (error) {
-        const errorMsg = document.createElement('p');
-        errorMsg.textContent = `error : ${error.message}`;
-        parentElement.appendChild(errorMsg);
-    }
-}
+      const newCard = document.createElement("div");
+      newCard.className = "card";
+      newCard.innerHTML = `
+            <div = "headerCard">
+            <p>${infoPokemon.name}</p>
+            <i class = "fa-sharp fa-regular fa-heart"></i>
+            </div>
+            <img class="pokemonImg" src ="${infoPokemon.sprites.other["home"].front_default}">
+            <div>
+            <p>Level: ${infoPokemon.base_experience}</p>
+            <button class = "btnBuy">Buy</button> 
+            `;
+      container.appendChild(newCard);
+    });
+  } catch (error) {
+    const errorMsg = document.createElement("p");
+    errorMsg.textContent = `error : ${error.message}`;
+    parentElement.appendChild(errorMsg);
+  }
+};
 
 fetchApi(url);
